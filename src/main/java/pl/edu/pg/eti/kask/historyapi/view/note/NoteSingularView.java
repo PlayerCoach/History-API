e0ea5fefc65c480dbf8b989ebd76d6759de74408
Player/Coach <olafinski14@gmail.com>
2025-11-13 18:38:38 +0100

    Final final final final final JPA working

--- src/main/java/pl/edu/pg/eti/kask/historyapi/view/note/NoteSingularView.java
+++ src/main/java/pl/edu/pg/eti/kask/historyapi/view/note/NoteSingularView.java
@@ -7,11 +7,8 @@ import jakarta.inject.Named;
 import lombok.Getter;
 import lombok.Setter;
 import pl.edu.pg.eti.kask.historyapi.historicalfigure.entity.HistoricalFigure;
-import pl.edu.pg.eti.kask.historyapi.historicalfigure.service.HistoricalFigureService;
 import pl.edu.pg.eti.kask.historyapi.note.entity.Note;
 import pl.edu.pg.eti.kask.historyapi.note.service.NoteService;
-import pl.edu.pg.eti.kask.historyapi.user.entity.User;
-import pl.edu.pg.eti.kask.historyapi.user.service.UserService;
 
 import java.io.Serializable;
 import java.util.UUID;
@@ -22,18 +19,12 @@ public class NoteSingularView implements Serializable {
     @Inject
     private NoteService noteService;
 
-    @Inject
-    private UserService userService;
-
-    @Inject
-    private HistoricalFigureService figureService;
 
     @Getter
     @Setter
     private UUID noteId;
 
     private Note note;
-    private User author;
     private HistoricalFigure figure;
 
 
